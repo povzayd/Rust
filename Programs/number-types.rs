c0234230c23eb3082003af1192c57bcf12e3c46c
Zayd <166023703+povzayd@users.noreply.github.com>
2025-09-27 01:19:05 +0530

    Create number-types.rs

---
+++ Programs/number-types.rs
@@ -0,0 +1,9 @@
+//Different numbers in rust
+fn main(){
+let x: i8 = -5; //no. with size of 8 bits if you dont mention 8 bits will default take 32 bits. Can be both positive or negative.
+let y: u32 = 1000; //can only be positive.
+let z: f32 = 1000.23; //float no.'s
+print!("x: {}", x);
+print!("y: {}", y);
+print!("z: {}", z);
+}
